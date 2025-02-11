7ff3b2b6a2c064d84fe219d7816e0ece240d0dab
Chris Daley <chebizarro@gmail.com>
2025-02-11 14:23:37 -0800

    Updated package ID

--- android/app/src/main/kotlin/com/example/let_him_cook/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/let_him_cook/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.let_him_cook
+package org.nsf.let_him_cook
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
